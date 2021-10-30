bf5ebb18edcd48b9917f8b61bf7b17b29f2f4387
Itamar Golan <golan.itamar@gmail.com>
2021-10-30 06:22:51 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
-
 import os
 
 # Clone GFPGAN and enter the GFPGAN folder
 
 os.system("git clone https://github.com/TencentARC/GFPGAN.git")  
-os.system("cd GFPGAN")  
+# os.system("cd GFPGAN")  
+os.chdir("GFPGAN")
+
 os.system("pip install -r requirements.txt")  
 os.system("pip install basicsr")  
 os.system("pip install gradio")  
@@ -17,6 +18,16 @@ import torch
 from PIL import Image
 import glob
 import random
+import shutil
+import os
+
+# os.chdir(path)
+
+upload_folder = 'inputs/upload'
+
+if os.path.isdir(upload_folder):
+    shutil.rmtree(upload_folder)
+os.makedirs(upload_folder, exist_ok=True)
 
 
 def clear_dir(path):
@@ -33,6 +44,7 @@ def locate(filename):
 
 # Define the main function
 def generate(img):
+  
     input_path = 'inputs/upload/'
     path_results = 'results/restored_faces/'
     clear_dir(input_path)
@@ -71,79 +83,48 @@ iface = gr.Interface(generate, inputs=[img], article = """
                      outputs=['image'], enable_queue=True, live=False , verbose = True ,allow_screenshot=False, allow_flagging=False, title = "Free Image Restoration using GFPGAN",
 css = """
 
-# * {
-# 	margin: 0;
-# 	padding: 0;
-# 	box-sizing: border-box;
-# 	}
-
-# html {
-#   font-color : white;
-# 	height: 2000px;
-# 	background-color: red;
-# 	background-image: url(https://upload.wikimedia.org/wikipedia/commons/6/64/Creaci%C3%B3n_de_Ad%C3%A1n_%28Miguel_%C3%81ngel%29.jpg);
-# 	background-size: 100% auto;
-# 	background-position: top center;
-# 	background-attachment: fixed;
-# 	}
-
-
-# body {
-# 	height: 100%;
-# 	background-size: 50% 50%;
-# 	mix-blend-mode: luminosity;
-# 	}
-
-@import url(https://fonts.googleapis.com/css?family=Fjalla+One);
-body{
-	background-image: url(https://upload.wikimedia.org/wikipedia/commons/6/64/Creaci%C3%B3n_de_Ad%C3%A1n_%28Miguel_%C3%81ngel%29.jpg);
-}
-html {
-  font-family: 'Fjalla One', sans-serif;
-  color: firebrick;
-  width: 100%;
-  height: 150%;
-  background: rgb(169, 3, 41);
-
-  overflow: hidden;
-  position: bottom
-  # background: rgb(169, 3, 41);
-  /* Old browsers */
-  /* IE9 SVG, needs conditional override of 'filter' to 'none' */
-  # background: radial-gradient(ellipse at center, rgba(169, 3, 41, 1) 0%, rgba(143, 2, 34, 1) 44%, rgba(109, 0, 25, 1) 100%);
-}
-
-.rainbow-text {
-  position: absolute;
-  top: 50%;
-  color : blue
-
-  display: block;
-  left: 50%;
-  margin-left: -99px;
-  margin-top: -100px;
-}
-
-.title-text {
-  text-align: center;
-}
-
-.title-text span.letters {
-  font-size: 6rem;
-}
-
-.letters {
-  color: #FFF;
-  text-shadow: 2px 2px 6px #333;
-  animation: color-changer 8s infinite;
-  transition: color-changer ease-in-out;
-  @for $i from 1 through 7 {
-    &:nth-of-type(#{$i}) {
-      animation-delay: #{$i * 100}ms;
-    }
-  }
-}
-
-
-""")
-iface.launch(share=True)
+	@import url(https://fonts.googleapis.com/css?family=Fjalla+One);
+	body{
+		background-image: url(https://upload.wikimedia.org/wikipedia/commons/6/64/Creaci%C3%B3n_de_Ad%C3%A1n_%28Miguel_%C3%81ngel%29.jpg);
+	}
+	html {
+	  font-family: 'Fjalla One', sans-serif;
+	  color: firebrick;
+	  width: 100%;
+	  height: 150%;
+	  background: rgb(169, 3, 41);
+	  overflow: hidden;
+	  position: bottom
+	  # background: rgb(169, 3, 41);
+	  /* Old browsers */
+	  /* IE9 SVG, needs conditional override of 'filter' to 'none' */
+	  # background: radial-gradient(ellipse at center, rgba(169, 3, 41, 1) 0%, rgba(143, 2, 34, 1) 44%, rgba(109, 0, 25, 1) 100%);
+	}
+	.rainbow-text {
+	  position: absolute;
+	  top: 50%;
+	  color : blue
+	  display: block;
+	  left: 50%;
+	  margin-left: -99px;
+	  margin-top: -100px;
+	}
+	.title-text {
+	  text-align: center;
+	}
+	.title-text span.letters {
+	  font-size: 6rem;
+	}
+	.letters {
+	  color: #FFF;
+	  text-shadow: 2px 2px 6px #333;
+	  animation: color-changer 8s infinite;
+	  transition: color-changer ease-in-out;
+	  @for $i from 1 through 7 {
+	    &:nth-of-type(#{$i}) {
+	      animation-delay: #{$i * 100}ms;
+	    }
+	  }
+	}
+	""")
+iface.launch()
